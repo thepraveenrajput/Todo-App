c59ea85662b294c9ca9610f74953bf07957c6287
Praveen Singh <157791428+thepraveenrajput@users.noreply.github.com>
2024-12-21 11:16:25 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ function App() {
     <>
       <Navbar />
       <div className='mx:3 container md:mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh] md:w-[50%]'>
-        <h1 className='font-bold text-center text-3xl'>itask - Manage your todos at one place</h1>
+        <h1 className='font-bold text-center text-3xl'>iTask - Manage your Todos at one place</h1>
         <div className="addTodo my-5 flex flex-col gap-4">
           <h2 className='text-2xl font-bold'>Add a Todo</h2>
 
